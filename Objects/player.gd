f7c87062c6523ed6632a4858abca7856b85a586e
TaelsDaFoox <bazmulligan@gmail.com>
2025-11-29 02:36:53 -0800

    more level design + fastfall + gravity fixes
    updated level skybox and textures, and extended it a bit, also holding crouch midair makes you fall faster, also fixed the gravity constants for tricking and not tricking being swapped and also it not even tracking which one it should be using correctly

--- Objects/player.gd
+++ Objects/player.gd
@@ -16,6 +16,7 @@ var turnvel = 0.0
 @onready var noSlopeArea = $NoSlopeAngle
 @export var linkedUI: Control
 @export var trickBoost = 80.0
+@export var fastFallGravity = 8.0
 var trickFailWait = 0.0
 var slopeDir := 0.0
 var extraBoost = 0.0
@@ -28,6 +29,7 @@ func _ready() -> void:
 		linkedUI.linkedPlayer=self
 	anim.play("Board",0.25,0.0)
 func _physics_process(delta: float) -> void:
+	print(trickState)
 	if linkedUI and linkedUI.QTEactive:
 		linkedUI.QTEinput(input)
 	trickFailWait = move_toward(trickFailWait,0.0,delta)
@@ -73,20 +75,26 @@ func _physics_process(delta: float) -> void:
 		var slopeAng = Vector3.UP-get_floor_normal()
 		model.global_rotation.x = slopeAng.z
 		model.global_rotation.z = slopeAng.x
-		trickState=false
 	else:
 		if trickState:
-			velocity.y-=gravity
-		else:
 			velocity.y-=trickGravity
+		else:
+			if input.is_action_pressed("Crouch"):
+				velocity.y-=fastFallGravity
+			else:
+				velocity.y-=gravity
 		model.rotation.x=lerp_angle(rotation.x,0.0,delta*10)
 		model.rotation.z=lerp_angle(rotation.z,0.0,delta*10)
 	collider.global_rotation=Vector3.ZERO
 	move_and_slide()
-	extraBoost=move_toward(extraBoost,0.0,delta*20)
+	if is_on_floor():
+		extraBoost=move_toward(extraBoost,0.0,delta*40)
+	else:
+		extraBoost=move_toward(extraBoost,0.0,delta*7)
 	#print(trickFailWait)
 
 func tricked():
+	trickState=false
 	anim.play("Trick",0.1,1.5)
 	velocity.y=10.0
 	print("trick1!!")
