0b72bc75cae2d3c169883f0f226c20ea5c9358eb
TaelsDaFoox <bazmulligan@gmail.com>
2025-11-28 16:55:36 -0800

    splitscreen matches player count
    splitscreen screens actually match the players connected and there's a background to fill the empty space in 3P... but the aspect ratio is screwed up in 2P rn

---
+++ Scenes/Courses/filler_bg_scroll.gd
@@ -0,0 +1,6 @@
+extends TextureRect
+func _process(delta: float) -> void:
+	position.x -= delta*50
+	position.y +=delta*25
+	position.x = fmod(position.x,251.0)
+	position.y = fmod(position.y, 216.0)
--- Scenes/Courses/player_init.gd
+++ Scenes/Courses/player_init.gd
@@ -1,14 +1,21 @@
 extends Node
 @export var spawnPos:Vector3 = Vector3(0,1.5,0)
 @onready var cam = $"../SubViewport/Camera3D"
+@onready var cams = [$"../SubViewport/Camera3D",$"../SubViewport2/Camera3D",$"../SubViewport3/Camera3D",$"../SubViewport4/Camera3D"]
+@onready var uis = [$"../SubViewport/RaceUI",$"../SubViewport2/RaceUI",$"../SubViewport3/RaceUI",$"../SubViewport4/RaceUI"]
+@onready var screens = [$"../GridContainer/SplitscreenRect1",$"../GridContainer/SplitscreenRect2",$"../GridContainer/SplitscreenRect3",$"../GridContainer/SplitscreenRect4"]
 var player = load("res://Objects/player.tscn")
 @onready var ui = $"../SubViewport/RaceUI"
 @onready var screen1 = $"../GridContainer/SplitscreenRect1"
+@onready var screenContainer = $"../GridContainer"
 func _ready() -> void:
-	for i in PlayerManager.playerDevices:
+	for i in PlayerManager.playerDevices.size():
 		var spawnplr = player.instantiate()
-		spawnplr.position = spawnPos
-		spawnplr.input = DeviceInput.new(i)
+		spawnplr.position = spawnPos+Vector3(2.0,0.0,0.0)*i
+		spawnplr.input = DeviceInput.new(PlayerManager.playerDevices[i])
 		get_parent().call_deferred("add_child",spawnplr)
-		cam.player = spawnplr
-		spawnplr.linkedUI = ui
+		cams[i].player = spawnplr
+		spawnplr.linkedUI = uis[i]
+	for i in 4:
+		screens[i].visible = i<PlayerManager.playerDevices.size()
+	screenContainer.columns=ceil(PlayerManager.playerDevices.size()/2.0)
